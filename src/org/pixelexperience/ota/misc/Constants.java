dbc55d5e175b8ee5b28601c8a7b6d46c7632b1c7
Luke Williams <expressluke@turbox.uk>
2022-04-10 07:43:52 +0000

    Updates: Prepare for Unofficial OTA

--- src/org/pixelexperience/ota/misc/Constants.java
+++ src/org/pixelexperience/ota/misc/Constants.java
@@ -21,7 +21,7 @@ public final class Constants {
     public static final String AB_PAYLOAD_PROPERTIES_PATH = "payload_properties.txt";
     public static final String PREF_MOBILE_DATA_WARNING = "pref_mobile_data_warning";
     public static final String UNCRYPT_FILE_EXT = ".uncrypt";
-    public static final String PROP_BUILD_DATE = "org.pixelexperience.build_date_utc";
+    public static final String PROP_BUILD_DATE = "ro.build.date.utc";
     public static final String PROP_BUILD_TYPE = "org.pixelexperience.build_type";
     public static final String PROP_RECOVERY_UPDATE = "persist.sys.recovery_update";
     public static final String PREF_CURRENT_PERSISTENT_STATUS = "current_persistent_status";
@@ -30,9 +30,8 @@ public final class Constants {
     static final String PROP_AB_DEVICE = "ro.build.ab_update";
     static final String PROP_DEVICE = "org.pixelexperience.device";
     static final String PROP_BUILD_VERSION = "org.pixelexperience.version";
-    static final String OTA_URL = "https://download.pixelexperience.org/ota_v5/%s/%s";
-    static final String OTA_CI_URL = "https://download.pixelexperience.org/ota_ci/%s/%s";
-    static final String MAINTAINER_URL = "https://download.pixelexperience.org/team/%s";
-    static final String DOWNLOAD_WEBPAGE_URL = "https://download.pixelexperience.org/changelog/%s/%s";
+    static final String OTA_URL = "https://raw.githubusercontent.com/PixelExperience-Exynos/OTA/master/12.1/%s.json";
+    static final String MAINTAINER_URL = "https://github.com/PixelExperience-Exynos/OTA";
+    static final String DOWNLOAD_WEBPAGE_URL = "https://raw.githubusercontent.com/PixelExperience-Exynos/OTA/master/12.1/changelog/changelog_%s.txt";
     static final String EXPORT_PATH = "PixelExperience-Updates/";
 }
